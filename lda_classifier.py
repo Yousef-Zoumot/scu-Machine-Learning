8926f7bba2d047eae109b47bd419d8f900b6bf9c
Yousef-Zoumot <yzoumot17@gmail.com>
2018-02-01 13:47:05 -0800

    include text file of dataset for iris, separate data into training and testing, compute mean

--- lda_classifier.py
+++ lda_classifier.py
@@ -1,31 +1,90 @@
 #Import mathematical functions
 import numpy as np
 
-import requests
+#open text file with data points
+text_file = open("dataset_iris.txt", "r")
+#split each line
+lines = text_file.read().split("\n")
+text_file.close()
 
-link = "http://www.cse.scu.edu/~yfang/coen140/iris.data"
-f = requests.get(link)
+print(lines)
+print(len(lines)) #151
+total_data_instances = len(lines) - 1 #150
+print(total_data_instances)
 
-print(f.text)
+#[[0 for x in range(columns)] for y in range(rows)]
+dataset_iris = [[0 for x in range(5)] for y in range(150)]
+for i in range(total_data_instances):
+    #split data points of each instance
+    #[sepal length, sepal width, pedal length, pedal width, class]
+    dataset_iris[i] = lines[i].split(",")
 
-data = f.text
+print(dataset_iris[1][0])
 
-print(data)
+#separate data into 80% training and 20% testing
+iris_setosa_training = [[0 for x in range(5)] for y in range(40)]
+iris_setosa_test = [[0 for x in range(5)] for y in range(10)]
 
-data1 = data.split("\n")
+iris_versicolor_training = [[0 for x in range(5)] for y in range(40)]
+iris_versicolor_test = [[0 for x in range(5)] for y in range(10)]
 
-print(data1[149])
-#
-# iris-setosa-training = data1[0:39]
-# iris-setosa-test = data1[40:49]
-#
-# iris-versicolor-training = data1[50:89]
-# iris-versicolor-test = data1[90:99]
-#
-# iris-virginica-training = data1[100:139]
-# iris-virginica-test = data1[140:149]
+iris_virginica_training = [[0 for x in range(5)] for y in range(40)]
+iris_virginica_test = [[0 for x in range(5)] for y in range(10)]
 
-for i in range(0, 150):
-    data1[i] = data1[i].split(",")
+setosa_mean = [[0 for x in range(1)] for y in range(4)]
 
-print(data1[149])
+versicolor_mean = [[0 for x in range(1)] for y in range(4)]
+
+virginica_mean = [[0 for x in range(1)] for y in range(4)]
+
+for i in range(0, 40):
+    setosa_mean[0][0] += float(dataset_iris[i][0])
+    setosa_mean[1][0] += float(dataset_iris[i][1])
+    setosa_mean[2][0] += float(dataset_iris[i][2])
+    setosa_mean[3][0] += float(dataset_iris[i][3])
+    # print(setosa_mean)
+    iris_setosa_training[i] = dataset_iris[i]
+
+print(setosa_mean)
+setosa_mean[0][0] = float(setosa_mean[0][0]) / 40
+setosa_mean[1][0] = float(setosa_mean[1][0]) / 40
+setosa_mean[2][0] = float(setosa_mean[2][0]) / 40
+setosa_mean[3][0] = float(setosa_mean[3][0]) / 40
+print(setosa_mean)
+
+for i in range(0, 10):
+    iris_setosa_test[i] = dataset_iris[i + 40]
+
+for i in range(0, 40):
+    versicolor_mean[0][0] += float(dataset_iris[i + 50][0])
+    versicolor_mean[1][0] += float(dataset_iris[i + 50][1])
+    versicolor_mean[2][0] += float(dataset_iris[i + 50][2])
+    versicolor_mean[3][0] += float(dataset_iris[i + 50][3])
+    iris_versicolor_training[i] = dataset_iris[i + 50]
+
+print(versicolor_mean)
+versicolor_mean[0][0] = float(versicolor_mean[0][0]) / 40
+versicolor_mean[1][0] = float(versicolor_mean[1][0]) / 40
+versicolor_mean[2][0] = float(versicolor_mean[2][0]) / 40
+versicolor_mean[3][0] = float(versicolor_mean[3][0]) / 40
+print(versicolor_mean)
+
+for i in range(0, 10):
+    iris_versicolor_test[i] = dataset_iris[i + 90]
+
+for i in range(0, 40):
+    virginica_mean[0][0] += float(dataset_iris[i + 100][0])
+    virginica_mean[1][0] += float(dataset_iris[i + 100][1])
+    virginica_mean[2][0] += float(dataset_iris[i + 100][2])
+    virginica_mean[3][0] += float(dataset_iris[i + 100][3])
+    iris_virginica_training[i] = dataset_iris[i + 100]
+
+print(virginica_mean)
+virginica_mean[0][0] = float(virginica_mean[0][0]) / 40
+virginica_mean[1][0] = float(virginica_mean[1][0]) / 40
+virginica_mean[2][0] = float(virginica_mean[2][0]) / 40
+virginica_mean[3][0] = float(virginica_mean[3][0]) / 40
+print(virginica_mean)
+
+for i in range(0, 10):
+    iris_virginica_test[i] = dataset_iris[i + 140]
